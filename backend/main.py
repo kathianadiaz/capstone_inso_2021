e205852673efecc13b5d709b8de7ae6b5f21cb22
Samuel Diaz Bidot <samuel.diaz9@upr.edu>
2021-09-07 23:28:24 -0400

    Configured dev database connection

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,22 @@
+import models
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from database import engine, get_db
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= ["http://localhost", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+models.Base.metadata.create_all(bind=engine)
+
 
 
 @app.get("/")
