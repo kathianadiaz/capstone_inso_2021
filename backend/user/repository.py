b0f0e9424c65122b6d5a5c6b7d10a1e5f03db599
kathiana.diaz <kathiana.diaz@upr.edu>
2021-09-27 12:55:39 -0400

    missing functions in user repo

--- backend/user/repository.py
+++ backend/user/repository.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.dialects.postgresql import insert
 from user import UserCreate, User
 import models
+from authentication import get_password_hash
 
 class UserRepository:
     '''Repository to perform CRUD operations on the `User` class in the database'''
@@ -9,8 +10,8 @@ class UserRepository:
     @staticmethod
     def add_user(db: Session, user: UserCreate) -> User:
         '''Add a `User` to the repository given a `UserCreate`'''
-        #TODO: hash password
-        db_user = models.User(name=user.name, username=user.username, email=user.email, password=user.password)
+        hashed_password = get_password_hash(user.password)
+        db_user = models.User(name=user.name, username=user.username, email=user.email, hashed_password=hashed_password)
         db.add(db_user)
         db.commit()
         db.refresh(db_user)
@@ -22,15 +23,33 @@ class UserRepository:
         return db.query(models.User).filter(models.User.u_id == user_id).first()
 
     @staticmethod
+    def get_users(db: Session, skip: int = 0, limit: int = 100):
+        '''Return all 'Users' with a limit of 100'''
+        return db.query(models.User).offset(skip).limit(limit).all()
+
+    @staticmethod
     def get_user_by_username(db: Session, username: str) -> User:
-        '''Return a `User` give a specific username'''
+        '''Return a `User` given a specific username'''
         return db.query(models.User).filter(models.User.username == username).first()
 
     @staticmethod
+    def get_user_by_email(db: Session, email: str):
+        '''Return a `User` given a specific email'''
+        return db.query(models.User).filter(models.User.email == email).first()
+
+    @staticmethod
     def user_already_exists(db: Session, username: str, email: str) -> bool:
-        '''Returns `True` if a user already exists. A duplicate User is considere as a user that contains the same email and username'''
+        '''Returns `True` if a user already exists. A duplicate User is considered as a user that contains the same email and username'''
         user = db.query(models.User).filter(models.User.username == username and models.User.email == email).first()
         if user:
             return True
         else:
             return False
+
+    @staticmethod
+    def delete_user_by_id(db: Session, user_id: int):
+        '''Delete `User` given a specific id'''
+        user = UserRepository.get_user(db, user_id)
+        db.delete(user)
+        db.commit()
+        return "USER DELETED"
