a2909ad5fa7eb3d47330db44eb04c2e4c69dd2cd
kathiana.diaz <kathiana.diaz@upr.edu>
2021-09-27 17:22:36 -0400

    auth implementation

--- backend/authentication/authentication.py
+++ backend/authentication/authentication.py
@@ -1,17 +1,71 @@
 from pydantic import BaseModel
 from typing import Optional
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt import JWTError, jwt
+from sqlalchemy.orm import Session
 
-# Pydantic schemas
-class UserBase(BaseModel):
-    name: str
-    email: str
+from user import UserInDB, User
+from user.repository import UserRepository
+from database import get_db
+from . import verify_password
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
     username: str
 
-class UserCreate(UserBase):
-    password: str
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate" : "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = UserRepository.get_user_by_username(db, token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
 
-class User(UserBase):
-    id: int
+def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
-    class Config:
-        orm_mode = True
+# Authenticates and returns the User
+def authenticate_user(db: Session, username: str, password: str):
+    user: UserInDB = UserRepository.get_user_by_username(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+# Utility function to generate a new access token
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp" : expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
