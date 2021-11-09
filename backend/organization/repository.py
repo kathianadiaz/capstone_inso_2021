ebacf82185f5902aca4de52dde5b349c22390f30
kathiana.diaz <kathiana.diaz@upr.edu>
2021-11-08 21:10:02 -0400

    search function implementation

--- backend/organization/repository.py
+++ backend/organization/repository.py
@@ -52,21 +52,25 @@ class OrganizationRepository:
             join(models.organization_administrator_assoc_table).\
             filter(models.organization_administrator_assoc_table.columns.user_id == u_id).all()
 
-    #TODO: get all organizations that contain the given keywords in their name or description
     @staticmethod
-    def get_organization_by_keyword(keywords:List[str], db:Session, skip: int = 0, limit: int = 25) -> List[Organization]:
-        '''Get all organizations that conatain a specific keyword in their description or name'''
-        pass
+    def search_organizations(keywords:List[str], db:Session, skip: int = 0, limit: int = 25) -> List[Organization]:
+        '''Get all organizations that contain a specific keyword in their description or name or a specific tag'''
+        organizations=set()
+        for keyword in keywords:
+            organizations.update(db.query(models.Organization).filter(models.Organization.name.contains(f'%{keyword}%') 
+                or models.Organization.description.contains(f'%{keyword}%')) 
+                or models.Organization.tags.contains([keyword])).offset(skip).limit(limit).all()
+        
+        return organizations if len(organizations) > 0 else None
 
-    #TODO: get all organizations that contain the given tags
     @staticmethod
     def get_organization_by_tags(tags: List[str], db: Session, skip: int = 0, limit: int = 25) -> List[Organization]:
         '''Get all organizations that contain the given tags'''
-        organizations = {}
+        organizations = set()
         for tag in tags:
-            organizations.add(db.query(models.Organization).filter(models.Organization.tags.contains([tag])).offset(skip).limit(limit).all())
+            organizations.update(db.query(models.Organization).filter(models.Organization.tags.contains([tag])).offset(skip).limit(limit).all())
         return organizations if len(organizations) > 0 else None
-
+    
     @staticmethod
     def delete_organization(o_id: str, user: User, db: Session) -> Optional[Organization]:
         '''Delete a specific `Organization` with the give id'''
@@ -99,11 +103,11 @@ class OrganizationRepository:
             return None
 
         # NOTE: error prone. There might be a better way to implement it
-        organization.name = new_organization.name
-        organization.description = new_organization.description
-        organization.tags = new_organization.tags
-        organization.status = new_organization.status
-        organization.highlight = new_organization.highlights
+        setattr(organization,"name",new_organization.name)
+        setattr(organization,"description",new_organization.description)
+        setattr(organization,"tags",new_organization.tags)
+        setattr(organization,"status",new_organization.status)
+        setattr(organization,"highlights",new_organization.highlights)
         db.commit()
 
         return new_organization
