f83b8c86c64934b3b7d5b7ec90084094a8a601d4
edisonzapata20 <edison.zapata@upr.edu>
2021-11-21 15:44:06 -0400

    Added filter button for tags,keyword, filteringFix

--- frontend/src/Components/OrganizationsList.js
+++ frontend/src/Components/OrganizationsList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Spinner } from "react-bootstrap";
+import { Button, Spinner, Dropdown } from "react-bootstrap";
 import "./OrganizationsList.scss";
 import OrganizationCard from "./OrganizationCard";
 import axios from "axios";
@@ -8,7 +8,7 @@ function OrganizationsList() {
   const [organizationsFilter, setorganizationsFilter] = useState([]);
   const [inputvalue, setInputValue] = useState("");
   const [spinner, setSpinner] = useState(true);
-
+  const [filtering, setFiltering] = useState("All");
   useEffect(() => {
     axios
       .get("http://localhost:8000/organization")
@@ -23,56 +23,112 @@ function OrganizationsList() {
       });
   }, []);
 
-  const filterOrgs = (e) => {
-    const input = e.target.value;
-    // console.log(e.target.value);
+  // const filterOrgs = (e) => {
+  //   const input = e.target.value;
+  //   console.log(e.target.value);
+  //   if (e.key === "Enter") {
+  //     const results = organizationsFilter.filter((organization) => {
+  //       return organization.name.toLowerCase().startsWith(input.toLowerCase());
+  //     });
+  //     setOrganizations(results);
+  //   } else {
+  //     setOrganizations(organizationsFilter);
+  //   }
+  //   console.log(organizationsFilter);
+  // };
+  const handleInputValue = (e) => {
+    setInputValue(e.target.value);
+    console.log(e.target.value);
     if (e.key === "Enter") {
-      const results = organizationsFilter.filter((organization) => {
-        return organization.name.toLowerCase().startsWith(input.toLowerCase());
-      });
-      setOrganizations(results);
+      setSpinner(true);
+      if (filtering === "All") {
+        let searchjson = {
+          tags: inputvalue,
+          keywords: inputvalue,
+        };
+        axios
+          .get(
+            `http://localhost:8000/organization/tags/${inputvalue}/keywords/${inputvalue}`,
+            searchjson
+          )
+          .then((response) => {
+            setOrganizations(response.data);
+            setSpinner(false);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
     } else {
       setOrganizations(organizationsFilter);
     }
   };
-
   const onChangeInput = (e) => {
     setInputValue(e.target.value);
   };
 
+  const handleSelect = (e) => {
+    setFiltering(e);
+  };
   const onClickFilter = () => {
-    console.log(inputvalue);
-    const results = organizationsFilter.filter((organization) => {
-      return organization.name
-        .toLowerCase()
-        .startsWith(inputvalue.toLowerCase());
-    });
-    setorganizationsFilter(results);
+    if (filtering === "All") {
+      let searchjson = {
+        tags: inputvalue,
+        keywords: inputvalue,
+      };
+      axios
+        .get(
+          `http://localhost:8000/organization/tags/${inputvalue}/keywords/${inputvalue}`,
+          searchjson
+        )
+        .then((response) => {
+          setOrganizations(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   };
   return (
     <div className="org-list-wrapper">
       <div className="org-search-wrapper">
         <div className="org-search">
+          <Dropdown onSelect={handleSelect}>
+            <Dropdown.Toggle
+              className="org-search-dropdown"
+              variant="success"
+              id="dropdown-basic"
+            >
+              {filtering}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item eventKey="All">All</Dropdown.Item>
+              <Dropdown.Item eventKey="Keywords">Keywords</Dropdown.Item>
+              <Dropdown.Item eventKey="Tags">Tags</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
           <input
             type="text"
             placeholder="Search organizations"
             className="org-search-input"
             value={inputvalue || ""}
             onChange={onChangeInput}
-            onKeyDown={filterOrgs}
+            onKeyDown={handleInputValue}
           />
+
           <Button className="btn org-search-button" onClick={onClickFilter}>
             Search
           </Button>
         </div>
       </div>
-      {console.log(organizations)}
+      {/* {console.log(organizations)} */}
 
       <div className="organizations-list">
         {!spinner && (
           <h3 className="organizations-list-header">
             <span className="blue-text">
-              Currently {organizations.length} available{" "}
+              Currently {organizations?.length} available{" "}
             </span>
             <span className="green-text">organizations</span>:
           </h3>
@@ -84,7 +140,7 @@ function OrganizationsList() {
               <span className="visually-hidden">Loading...</span>
             </Spinner>
           )}
-          {organizations && organizations.length > 0
+          {!spinner && organizations && organizations.length > 0
             ? organizations.map((organization, i) => (
                 <OrganizationCard
                   key={i}
