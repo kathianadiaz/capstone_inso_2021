fe9e6e8e16075c431a9d54fb2168e86e3e140a1e
Samuel Diaz Bidot <samuel.diaz9@upr.edu>
2021-12-06 17:55:10 -0400

    updated load test

--- backend/tests/load/locustfile.py
+++ backend/tests/load/locustfile.py
@@ -1,26 +1,21 @@
 import time
-import random 
 import uuid
 from locust import HttpUser, task, between, FastHttpUser
 from locust.exception import RescheduleTask
 
 #NOTE: Only run with testing db
 
-# class QuickstartUser(HttpUser):
 class QuickstartUser(FastHttpUser):
     wait_time = between(2, 10)
-    organizations = []
 
-    @task()
+    @task(5)
     def get_organizations(self):
-        with self.client.get("/organiation",catch_response=True) as response:
+        with self.client.get("/organization",catch_response=True) as response:
             if response.status_code != 200:
                 raise RescheduleTask()
 
-    @task()
-    def add_getByID_edit_delete_organization(self):
-        o_id = None   
-
+    @task(2)
+    def create_organization(self):
         with self.client.post("/organization", 
             json={"name": "test","description": "tests","tags": ["s","tag"],"department": "INSO","status": False,"highlights": []},
             headers=self.headers,
@@ -30,34 +25,42 @@ class QuickstartUser(FastHttpUser):
                 response.failure("error creating organization")
                 raise RescheduleTask()
 
-            self.organizations.append(response.json()['o_id'])
-
-            o_id = response.json()['o_id']
-
-        time.sleep(2)
-        with self.client.get(f'/organization/{o_id}',name='/organization/{id}') as response:
-
-            if response.status_code != 200 and response.status_code != 404:
-                response.failure("error creating organization")
+    @task()
+    def edit_organization(self):
+        with self.client.get('/my-organizations', headers=self.headers, catch_response=True) as response:
+            if response.status_code != 200:
+                response.failure("error getting my organizations")
+            
+            if len(response.json()) == 0:
                 raise RescheduleTask()
 
-        time.sleep(5)
-        with self.client.put('/organization',
-            json={"o_id": f"{o_id}", "name": "test2","description4": "tests","tags": ["tag"],"department": "INSO","status": False,"highlights": []},
-            headers=self.headers) as response:
+            o_id = response.json()[0]["o_id"]
+
+        with self.client.put(f'/organization/{o_id}',
+            json={"name": "test2","description4": "tests","tags": ["tag"],"department": "INSO","status": False,"highlights": []},
+            headers=self.headers, name='/organization/{id}') as response:
 
             if response.status_code != 200:
                 response.failure("error editing organization")
                 raise RescheduleTask()
 
-        time.sleep(2)
-        # with self.client.delete(f'/organization/{o_id}', headers=self.headers, name='/organization/{id}',) as response:
-        with self.client.request('DELETE',f'/organization/{o_id}', headers=self.headers, name='/organization/{id}',) as response:
+    @task()
+    def delete_organization(self):
+        with self.client.get('/my-organizations', headers=self.headers, catch_response=True) as response:
             if response.status_code != 200:
-                response.failure("error deleting organization")
+                response.failure("error getting my organizations")
+            
+            if len(response.json()) == 0:
                 raise RescheduleTask()
 
-    @task()
+            o_id = response.json()[0]["o_id"]
+
+            with self.client.request('DELETE',f'/organization/{o_id}', headers=self.headers, name='/organization/{id}',) as response:
+                if response.status_code != 200:
+                    response.failure("error deleting organization")
+                    raise RescheduleTask()            
+
+    @task(5)
     def get_my_organizations(self):
         with self.client.get('/my-organizations', headers=self.headers, catch_response=True) as response:
             if response.status_code != 200:
@@ -67,6 +70,51 @@ class QuickstartUser(FastHttpUser):
                 raise RescheduleTask()
 
     @task()
+    def get_my_organizations_member(self):
+        with self.client.get('/my-organizations-member', headers=self.headers, catch_response=True) as response:
+            if response.status_code != 200:
+                response.failure("error getting my organizations")
+            
+            if len(response.json()) == 0:
+                raise RescheduleTask()
+
+    @task(2)
+    def add_member_info(self):
+        with self.client.get('/my-organizations', headers=self.headers, catch_response=True) as response:
+            if response.status_code != 200:
+                response.failure("error getting my organizations")
+            
+            if len(response.json()) == 0:
+                raise RescheduleTask()
+
+            o_id = response.json()[0]["o_id"]
+
+        with self.client.post(f'/organization/{o_id}/member-information',
+            json={"name": "test2","email": "tests@mail.com","links": ["tag"]},
+            headers=self.headers, name='/organization/{id}/member-information') as response:
+
+            if response.status_code != 200:
+                response.failure("error adding member info")
+                raise RescheduleTask()
+
+            m_id = response.json()["m_id"]
+
+        with self.client.get(f'/member-information/{m_id}', name='/meber-information') as response:
+            if response.status_code != 200:
+                response.failure("error getting member info")
+                raise RescheduleTask()
+
+    @task(2)
+    def add_member_info_user(self):
+        with self.client.post(f'/user/member-information',
+            json={"name": "test2","email": "tests@mail.com","links": ["tag"]},
+            headers=self.headers, name='/user/member-information') as response:
+
+            if response.status_code != 200:
+                response.failure("error adding member info")
+                raise RescheduleTask()
+
+    @task(2)
     def add_and_delete_organization_highlight(self):
         o_id = oh_id = None   
 
@@ -98,6 +146,7 @@ class QuickstartUser(FastHttpUser):
                 response.failure("error deleting organization highlight")
                 raise RescheduleTask()
 
+
     def on_start(self):
         i = uuid.uuid4()
 
@@ -105,7 +154,7 @@ class QuickstartUser(FastHttpUser):
             if response.status_code != 200:
                 response.failure(f'register: response.status_code = {response.status_code}, expected 200')
 
-        time.sleep(2)
+        # time.sleep(2)
         response = self.client.post("/token", {"username":f"user_{i}", "password":"password"})
 
         access_token = response.json()['access_token']
